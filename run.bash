ca4162e91e31c1b1d8339082da3210eb33e99e97
afisk <a@littleshoot.org>
2009-10-25 21:51:54 +0000

    More cleanups

--- run.bash
+++ run.bash
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-function die()
-{
+function die() {
   echo $*
   exit 1
 }
 
-mvn clean
+mvn clean || die "Could not clean?"
 mvn package || die "Could not package proxy"
-cd target || die "No target dir?"
-java -jar littleproxy-0.1.jar $* || die "Java process exited abnormally"
+java -jar target/littleproxy-0.1.jar $* || die "Java process exited abnormally"
