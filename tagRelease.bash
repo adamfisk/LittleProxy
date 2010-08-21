57f1760fea6a94f50109a54d8b4c9d3dbb1e9bbd
afisk <a@littleshoot.org>
2010-08-21 05:04:52 +0000

    Tweaked to check args

--- tagRelease.bash
+++ tagRelease.bash
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+ARGS=1  # One arg to script expected.
+
+if [ $# -ne "$ARGS" ]
+then
+  echo "Must include the version number"
+  exit 1
+fi
+
 RELEASE_VERSION=$1
 svn copy "http://svn.littleshoot.org/svn/littleproxy/trunk" "http://svn.littleshoot.org/svn/littleproxy/tags/littleproxy-${RELEASE_VERSION}" -m "Tag for LittleProxy release ${RELEASE_VERSION}"
