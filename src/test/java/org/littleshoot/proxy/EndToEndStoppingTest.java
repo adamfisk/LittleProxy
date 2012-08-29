a12453d4082b170a086e0ba9a33cbaa850c166f1
Adam Fisk <a@littleshoot.org>
2012-08-29 12:19:36 -0700

    Added test from nasis #37

--- src/test/java/org/littleshoot/proxy/EndToEndStoppingTest.java
+++ src/test/java/org/littleshoot/proxy/EndToEndStoppingTest.java
@@ -2,6 +2,8 @@ package org.littleshoot.proxy;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.TimeUnit;
+
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -11,6 +13,11 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.jboss.netty.handler.codec.http.HttpRequest;
 import org.junit.Test;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
  * End to end test making sure the proxy is able to service simple HTTP 
@@ -20,7 +27,7 @@ import org.junit.Test;
 public class EndToEndStoppingTest {
 
     @Test
-    public void test() throws Exception {
+    public void testWithHttpClient() throws Exception {
         
         final int PROXY_PORT = 10200;
         final HttpClient client = new DefaultHttpClient();
@@ -46,6 +53,37 @@ public class EndToEndStoppingTest {
         assertEquals(200, response.getStatusLine().getStatusCode());
         EntityUtils.consume(response.getEntity());
         proxy.stop();
+    }
+    
+    @Test
+    public void testWithWebDriver() throws Exception {
+        int port = 9090;
+        HttpProxyServer proxyServer = new DefaultHttpProxyServer(port);
+        proxyServer.start();
+
+        Proxy proxy = new Proxy();
+        proxy.setProxyType(Proxy.ProxyType.MANUAL);
+        String proxyStr = String.format("localhost:%d", port);
+        proxy.setHttpProxy(proxyStr);
+        proxy.setSslProxy(proxyStr);
+
+        DesiredCapabilities capability = DesiredCapabilities.firefox();
+        capability.setCapability(CapabilityType.PROXY, proxy);
+
+        final String urlString = "http://www.yahoo.com/";
+        
+        // Note this will actually launch a browser!!
+        final WebDriver driver = new FirefoxDriver(capability);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+        driver.get(urlString);
+        final String source = driver.getPageSource();
+        
+        // Just make sure it got something within reason.
+        assertTrue(source.length() > 100);
+        driver.close();
+
+        proxyServer.stop();
     }
 
 }
