57591471f1cf9958bef46e0ab96c5532068994bc
Adam Fisk <a@littleshoot.org>
2013-01-15 18:49:48 -0800

    added oome handling, bumped memory

--- run.bash
+++ run.bash
@@ -9,7 +9,7 @@ mvn package -Dmaven.test.skip=true || die "Could not package"
 fullPath=`dirname $0`
 jar=`find $fullPath/target/*with-dependencies.jar`
 cp=`echo $jar | sed 's,./,'$fullPath'/,'`
-javaArgs="-server -Xmx600m -jar "$cp" $*"
+javaArgs="-server -XX:+HeapDumpOnOutOfMemoryError -Xmx800m -jar "$cp" $*"
 
 echo "Running using Java on path at `which java` with args $javaArgs"
 java $javaArgs || die "Java process exited abnormally"
