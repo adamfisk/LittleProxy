6f4b922b65e30236971a4ab4af1eaa9dfcaff759
Ox To A Cart <ox.to.a.cart@gmail.com>
2013-08-20 17:52:37 -0500

    Fixed issue with large file uploads

--- run.bash
+++ run.bash
@@ -9,7 +9,7 @@ mvn package -Dmaven.test.skip=true || die "Could not package"
 fullPath=`dirname $0`
 jar=`find $fullPath/target/littleproxy*-littleproxy-shade.jar`
 cp=`echo $jar | sed 's,./,'$fullPath'/,'`
-javaArgs="-server -XX:+HeapDumpOnOutOfMemoryError -Xmx256m -jar "$cp" $*"
+javaArgs="-server -XX:+HeapDumpOnOutOfMemoryError -Xmx800m -jar "$cp" $*"
 
 echo "Running using Java on path at `which java` with args $javaArgs"
 java $javaArgs || die "Java process exited abnormally"
