a506adac83bf84fafe1d23ed38717f0d5d7f9424
Ox To A Cart <ox.to.a.cart@gmail.com>
2013-11-12 10:51:37 -0600

    Improved comments on HttpFiltersSource

--- src/main/java/org/littleshoot/proxy/HttpFiltersSource.java
+++ src/main/java/org/littleshoot/proxy/HttpFiltersSource.java
@@ -1,5 +1,6 @@
 package org.littleshoot.proxy;
 
+import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
 
@@ -20,7 +21,11 @@ public interface HttpFiltersSource {
      * Indicate how many (if any) bytes to buffer for incoming
      * {@link HttpRequest}s. A value of 0 or less indicates that no buffering
      * should happen and that messages will be passed to the {@link HttpFilters}
-     * request filtering methods chunk by chunk.
+     * request filtering methods chunk by chunk. A positive value will cause
+     * LittleProxy to try an create a {@link FullHttpRequeste} using the data
+     * received from the client, with its content already decompressed (in case
+     * the client was compressing it). If the request size exceeds the maximum
+     * buffer size, the request will fail.
      * 
      * @return
      */
@@ -30,7 +35,11 @@ public interface HttpFiltersSource {
      * Indicate how many (if any) bytes to buffer for incoming
      * {@link HttpResponse}s. A value of 0 or less indicates that no buffering
      * should happen and that messages will be passed to the {@link HttpFilters}
-     * response filtering methods chunk by chunk.
+     * response filtering methods chunk by chunk. A positive value will cause
+     * LittleProxy to try an create a {@link FullHttpResponse} using the data
+     * received from the server, with its content already decompressed (in case
+     * the server was compressing it). If the response size exceeds the maximum
+     * buffer size, the response will fail.
      * 
      * @return
      */
