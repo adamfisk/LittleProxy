f0dc8afc65547b8003393c495d2f3ed993143dfa
KevG <kevin@infofinity.com>
2014-01-20 12:41:10 -0800

    Minor doc update

--- src/main/java/org/littleshoot/proxy/HttpFilters2.java
+++ src/main/java/org/littleshoot/proxy/HttpFilters2.java
@@ -8,7 +8,6 @@ import io.netty.handler.codec.http.HttpResponse;
  * <p>
  * Additional filters on top of {@link org.littleshoot.proxy.HttpFilters}
  * </p>
- *
  */
 public interface HttpFilters2 extends HttpFilters {
     String parseHostAndPort(final HttpRequest httpRequest);
