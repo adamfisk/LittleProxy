e5d3ada80847872537d6125e11ea6769e17cfba0
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-01-31 09:07:10 -0600

    #131 Added basic ChainedMain for performance comparison

---
+++ src/test/java/org/littleshoot/proxy/ChainedMain.java
@@ -0,0 +1,90 @@
+package org.littleshoot.proxy;
+
+import io.netty.handler.codec.http.HttpRequest;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.Queue;
+
+import javax.net.ssl.SSLEngine;
+
+import org.littleshoot.proxy.extras.SelfSignedSslEngineSource;
+import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
+
+/**
+ * <p>
+ * This simple program runs two local LittleProxies that talk to each other
+ * directly. After running this program, you can do this:
+ * </p>
+ * 
+ * <pre>
+ * curl -x 127.0.0.1:8080 http://www.google.com
+ * </pre>
+ */
+public class ChainedMain {
+    protected static InetAddress LOCALHOST;
+
+    static {
+        try {
+            LOCALHOST = InetAddress.getByName("127.0.0.1");
+        } catch (Exception e) {
+            LOCALHOST = null;
+            System.err.println("Unable to parse LOCALHOSt");
+        }
+    }
+
+    protected static final int LITTLEPROXY_DOWNSTREAM_PORT = 8080;
+    protected static final int LITTLEPROXY_UPSTREAM_PORT = 8083;
+
+    public static void main(String[] args) throws Exception {
+        new FTEMain().run();
+    }
+
+    public void run() throws Exception {
+        final SslEngineSource sslSource = new SelfSignedSslEngineSource();
+        // Downstream LittleProxy connects to fteproxy client
+        DefaultHttpProxyServer
+                .bootstrap()
+                .withName("Downstream")
+                .withAddress(
+                        new InetSocketAddress(LOCALHOST,
+                                LITTLEPROXY_DOWNSTREAM_PORT))
+                .withConnectTimeout(5000)
+                .withChainProxyManager(new ChainedProxyManager() {
+                    @Override
+                    public void lookupChainedProxies(HttpRequest httpRequest,
+                            Queue<ChainedProxy> chainedProxies) {
+                        chainedProxies.add(new ChainedProxyAdapter() {
+                            @Override
+                            public InetSocketAddress getChainedProxyAddress() {
+                                return new InetSocketAddress(LOCALHOST,
+                                        LITTLEPROXY_UPSTREAM_PORT);
+                            }
+
+                            @Override
+                            public boolean requiresEncryption() {
+                                return true;
+                            }
+
+                            @Override
+                            public SSLEngine newSslEngine() {
+                                return sslSource.newSslEngine();
+                            }
+                        });
+                    }
+                })
+                .start();
+
+        // Upstream LittleProxy
+        DefaultHttpProxyServer
+                .bootstrap()
+                .withName("Upstream")
+                .withSslEngineSource(sslSource)
+                .withAddress(
+                        new InetSocketAddress(LOCALHOST,
+                                LITTLEPROXY_UPSTREAM_PORT))
+                .start();
+
+    }
+
+}
--- src/test/java/org/littleshoot/proxy/FTEMain.java
+++ src/test/java/org/littleshoot/proxy/FTEMain.java
@@ -1,11 +1,5 @@
 package org.littleshoot.proxy;
 
-import io.netty.handler.codec.http.HttpRequest;
-
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.util.Queue;
-
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecuteResultHandler;
 import org.apache.commons.exec.DefaultExecutor;
@@ -13,62 +7,30 @@ import org.apache.commons.exec.ExecuteWatchdog;
 import org.apache.commons.exec.Executor;
 import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.commons.exec.ShutdownHookProcessDestroyer;
-import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
 
 /**
- * <p>This simple program runs two local LittleProxies that talk to each other via
- * two local fteproxy instances (client and server).  After running this program,
- * you can do this:</p>
+ * <p>
+ * This simple program runs two local LittleProxies that talk to each other via
+ * two local fteproxy instances (client and server). After running this program,
+ * you can do this:
+ * </p>
  * 
  * <pre>
  * curl -x 127.0.0.1:8080 http://www.google.com
  * </pre>
  */
-public class FTEMain {
+public class FTEMain extends ChainedMain {
     private static final String FTEPROXY_LOCATION = "/Applications/TorBrowser_en-US.app/Contents/MacOS/fteproxy";
-    private static InetAddress LOCALHOST;
-
-    static {
-        try {
-            LOCALHOST = InetAddress.getByName("127.0.0.1");
-        } catch (Exception e) {
-            LOCALHOST = null;
-            System.err.println("Unable to parse LOCALHOSt");
-        }
-    }
-
-    private static final int LITTLEPROXY_DOWNSTREAM_PORT = 8080;
     private static final int FTEPROXY_CLIENT_PORT = 8081;
     private static final int FTEPROXY_SERVER_PORT = 8082;
-    private static final int LITTLEPROXY_UPSTREAM_PORT = 8083;
 
     public static void main(String[] args) throws Exception {
         new FTEMain().run();
     }
 
     public void run() throws Exception {
-        // Downstream LittleProxy connects to fteproxy client
-        DefaultHttpProxyServer
-                .bootstrap()
-                .withName("Downstream")
-                .withAddress(
-                        new InetSocketAddress(LOCALHOST,
-                                LITTLEPROXY_DOWNSTREAM_PORT))
-                .withConnectTimeout(5000)
-                .withChainProxyManager(new ChainedProxyManager() {
-                    @Override
-                    public void lookupChainedProxies(HttpRequest httpRequest,
-                            Queue<ChainedProxy> chainedProxies) {
-                        chainedProxies.add(new ChainedProxyAdapter() {
-                            @Override
-                            public InetSocketAddress getChainedProxyAddress() {
-                                return new InetSocketAddress(LOCALHOST,
-                                        FTEPROXY_CLIENT_PORT);
-                            }
-                        });
-                    }
-                })
-                .start();
+        // Start LittleProxy servers
+        super.run();
 
         // fteproxy client
         fteProxy(
@@ -81,16 +43,6 @@ public class FTEMain {
                 "--mode", "server",
                 "--server_port", FTEPROXY_SERVER_PORT,
                 "--proxy_port", LITTLEPROXY_UPSTREAM_PORT);
-
-        // Upstream LittleProxy
-        DefaultHttpProxyServer
-                .bootstrap()
-                .withName("Upstream")
-                .withAddress(
-                        new InetSocketAddress(LOCALHOST,
-                                LITTLEPROXY_UPSTREAM_PORT))
-                .start();
-
     }
 
     private void fteProxy(Object... args) throws Exception {
