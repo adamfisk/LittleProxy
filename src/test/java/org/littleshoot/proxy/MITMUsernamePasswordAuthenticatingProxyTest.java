dd6fdc1e4bd2033c59d8cff9d00d80756ef12e9d
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-03-16 16:35:50 -0500

    #138 Fixed proxy authentication issue when MITM'ing

--- src/test/java/org/littleshoot/proxy/MITMUsernamePasswordAuthenticatingProxyTest.java
+++ src/test/java/org/littleshoot/proxy/MITMUsernamePasswordAuthenticatingProxyTest.java
@@ -3,7 +3,8 @@ package org.littleshoot.proxy;
 import org.littleshoot.proxy.extras.SelfSignedMitmManager;
 
 /**
- * Tests a single proxy that requires username/password authentication.
+ * Tests a single proxy that requires username/password authentication and that
+ * uses MITM.
  */
 public class MITMUsernamePasswordAuthenticatingProxyTest extends
         UsernamePasswordAuthenticatingProxyTest
@@ -16,7 +17,7 @@ public class MITMUsernamePasswordAuthenticatingProxyTest extends
                 .withManInTheMiddle(new SelfSignedMitmManager())
                 .start();
     }
-    
+
     @Override
     protected boolean isMITM() {
         return true;
