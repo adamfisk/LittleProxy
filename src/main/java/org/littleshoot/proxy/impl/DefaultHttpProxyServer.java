045bc3e2903a661edfe3865b3e9eb64c1640eef0
KevG <kevin@infofinity.com>
2014-04-09 15:08:32 -0700

    Throw an exception during server bind if it fails.

--- src/main/java/org/littleshoot/proxy/impl/DefaultHttpProxyServer.java
+++ src/main/java/org/littleshoot/proxy/impl/DefaultHttpProxyServer.java
@@ -325,13 +325,25 @@ public class DefaultHttpProxyServer implements HttpProxyServer {
             throw new UnknownTransportProtocolError(transportProtocol);
         }
         serverBootstrap.childHandler(initializer);
-        serverBootstrap.bind(address).addListener(new ChannelFutureListener() {
+        ChannelFuture future = serverBootstrap.bind(address).addListener(new ChannelFutureListener() {
             @Override
             public void operationComplete(ChannelFuture future)
                     throws Exception {
-                registerChannel(future.channel());
+                if (future.isSuccess()) {
+                    registerChannel(future.channel());
+                }
             }
         }).awaitUninterruptibly();
+        if (future.isSuccess()) {
+            future.awaitUninterruptibly();
+        } else {
+            Throwable cause = future.cause();
+            if (cause != null) {
+                throw new RuntimeException(cause);
+            } else {
+                throw new RuntimeException("Error binding: no details.");
+            }
+        }
     }
 
     /**
