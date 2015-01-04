a48f0274630feb3d2b7eaffcf5886da321a0ada8
dawnbreaks <308181687@qq.com>
2015-01-04 09:40:50 +0800

    update start shell script

--- start.sh
+++ start.sh
@@ -12,4 +12,6 @@ jvmOptions="-server -XX:PermSize=24M -XX:MaxPermSize=64m -Xms128m -Xmx448m -XX:+
 
 printf "Starting service.....\n" 
 nohup java -Djava.net.preferIPv4Stack=true  $jvmOptions -cp $classpath   org.littleshoot.proxy.Launcher --port 443 --mitm 2>&1 >> $currentDir/log.txt  & 
-printf "Done..."; cecho " Ok"    boldgreen;
+printf "Done... Ok"
+
+tail -f log.txt 
