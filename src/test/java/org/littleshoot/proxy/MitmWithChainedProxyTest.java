32ad2c7be59fca1daf46f93b96a1f09c870e2524
MediumOne <ssivakumaraw@gmail.com>
2015-12-24 15:42:00 +0530

    Fixed some test cases related to MITM + chained proxy feature.

--- src/test/java/org/littleshoot/proxy/MitmWithChainedProxyTest.java
+++ src/test/java/org/littleshoot/proxy/MitmWithChainedProxyTest.java
@@ -108,35 +108,43 @@ public class MitmWithChainedProxyTest extends BaseChainedProxyTest {
     @Override
     public void testSimpleGetRequest() throws Exception {
         super.testSimpleGetRequest();
-        assertMethodSeenInRequestFilters(HttpMethod.GET);
-        assertMethodSeenInResponseFilters(HttpMethod.GET);
-        assertResponseFromFiltersMatchesActualResponse();
+        if (isChained() && !expectBadGatewayForEverything()) {
+            assertMethodSeenInRequestFilters(HttpMethod.GET);
+            assertMethodSeenInResponseFilters(HttpMethod.GET);
+            assertResponseFromFiltersMatchesActualResponse();
+        }
     }
 
     @Override
     public void testSimpleGetRequestOverHTTPS() throws Exception {
         super.testSimpleGetRequestOverHTTPS();
-        assertMethodSeenInRequestFilters(HttpMethod.CONNECT);
-        assertMethodSeenInRequestFilters(HttpMethod.GET);
-        assertMethodSeenInResponseFilters(HttpMethod.GET);
-        assertResponseFromFiltersMatchesActualResponse();
+        if (isChained() && !expectBadGatewayForEverything()) {
+            assertMethodSeenInRequestFilters(HttpMethod.CONNECT);
+            assertMethodSeenInRequestFilters(HttpMethod.GET);
+            assertMethodSeenInResponseFilters(HttpMethod.GET);
+            assertResponseFromFiltersMatchesActualResponse();
+        }
     }
 
     @Override
     public void testSimplePostRequest() throws Exception {
         super.testSimplePostRequest();
-        assertMethodSeenInRequestFilters(HttpMethod.POST);
-        assertMethodSeenInResponseFilters(HttpMethod.POST);
-        assertResponseFromFiltersMatchesActualResponse();
+        if (isChained() && !expectBadGatewayForEverything()) {
+            assertMethodSeenInRequestFilters(HttpMethod.POST);
+            assertMethodSeenInResponseFilters(HttpMethod.POST);
+            assertResponseFromFiltersMatchesActualResponse();
+        }
     }
     
     @Override
     public void testSimplePostRequestOverHTTPS() throws Exception {
         super.testSimplePostRequestOverHTTPS();
-        assertMethodSeenInRequestFilters(HttpMethod.CONNECT);
-        assertMethodSeenInRequestFilters(HttpMethod.POST);
-        assertMethodSeenInResponseFilters(HttpMethod.POST);
-        assertResponseFromFiltersMatchesActualResponse();
+        if (isChained() && !expectBadGatewayForEverything()) {
+            assertMethodSeenInRequestFilters(HttpMethod.CONNECT);
+            assertMethodSeenInRequestFilters(HttpMethod.POST);
+            assertMethodSeenInResponseFilters(HttpMethod.POST);
+            assertResponseFromFiltersMatchesActualResponse();
+        }
     }
     
     private void assertMethodSeenInRequestFilters(HttpMethod method) {
