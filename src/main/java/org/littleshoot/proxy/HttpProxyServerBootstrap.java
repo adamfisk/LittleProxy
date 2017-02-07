4ab98298795ea60bd58205d6551e63f7e14a4034
Jason Hoetger <jekh@outlook.com>
2017-02-06 16:42:07 -0800

    Added allowRequestsToOriginServer option to Bootstrap to allow handling origin-form requests

--- src/main/java/org/littleshoot/proxy/HttpProxyServerBootstrap.java
+++ src/main/java/org/littleshoot/proxy/HttpProxyServerBootstrap.java
@@ -302,6 +302,16 @@ public interface HttpProxyServerBootstrap {
     HttpProxyServerBootstrap withMaxChunkSize(int maxChunkSize);
 
     /**
+     * When true, the proxy will accept requests that appear to be directed at an origin server (i.e. the URI in the HTTP
+     * request will contain an origin-form, rather than an absolute-form, as specified in RFC 7230, section 5.3).
+     * This is useful when the proxy is acting as a gateway/reverse proxy. <b>Note:</b> This feature should not be
+     * enabled when running as a forward proxy; doing so may cause an infinite loop if the client requests the URI of the proxy.
+     *
+     * @param allowRequestToOriginServer when true, the proxy will accept origin-form HTTP requests
+     */
+    HttpProxyServerBootstrap withAllowRequestToOriginServer(boolean allowRequestToOriginServer);
+
+    /**
      * Sets the alias to use when adding Via headers to incoming and outgoing HTTP messages. The alias may be any
      * pseudonym, or if not specified, defaults to the hostname of the local machine. See RFC 7230, section 5.7.1.
      *
--- src/test/java/org/littleshoot/proxy/DirectRequestTest.java
+++ src/test/java/org/littleshoot/proxy/DirectRequestTest.java
@@ -1,6 +1,7 @@
 package org.littleshoot.proxy;
 
 import io.netty.handler.codec.http.DefaultHttpResponse;
+import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
@@ -13,10 +14,12 @@ import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
 import org.littleshoot.proxy.test.HttpClientUtil;
 
 import javax.net.ssl.SSLException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * This class tests direct requests to the proxy server, which causes endless
@@ -95,6 +98,48 @@ public class DirectRequestTest {
             Throwable cause = e.getCause();
             assertThat("Expected an SSL exception when attempting to perform an HTTPS GET directly to the proxy", cause, instanceOf(SSLException.class));
         }
+    }
+
+    @Test(timeout = 5000)
+    public void testAllowRequestToOriginServerWithOverride() {
+        // verify that the filter is hit twice: first, on the request from the client, without a Via header; and second, when the proxy
+        // forwards the request to itself
+        final AtomicBoolean receivedRequestWithoutVia = new AtomicBoolean();
+
+        proxyServer = DefaultHttpProxyServer.bootstrap()
+                .withPort(0)
+                .withAllowRequestToOriginServer(true)
+                .withProxyAlias("testAllowRequestToOriginServerWithOverride")
+                .withFiltersSource(new HttpFiltersSourceAdapter() {
+                    @Override
+                    public HttpFilters filterRequest(HttpRequest originalRequest) {
+                        return new HttpFiltersAdapter(originalRequest) {
+                            @Override
+                            public HttpResponse clientToProxyRequest(HttpObject httpObject) {
+                                if (httpObject instanceof HttpRequest) {
+                                    HttpRequest request = (HttpRequest) httpObject;
+                                    String viaHeader = request.headers().get(HttpHeaders.Names.VIA);
+                                    if (viaHeader != null && viaHeader.contains("testAllowRequestToOriginServerWithOverride")) {
+                                        return new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NO_CONTENT);
+                                    } else {
+                                        receivedRequestWithoutVia.set(true);
+                                    }
+                                }
+                                return null;
+                            }
+                        };
+                    }
+                })
+                .start();
+
+        int proxyPort = proxyServer.getListenAddress().getPort();
+
+        org.apache.http.HttpResponse response = HttpClientUtil.performHttpGet("http://localhost:" + proxyPort + "/originrequest", proxyServer);
+        int statusCode = response.getStatusLine().getStatusCode();
+
+        assertEquals("Expected to receive a 204 response from the filter", 204, statusCode);
+
+        assertTrue("Expected to receive a request from the client without a Via header", receivedRequestWithoutVia.get());
     }
 
     private void startProxyServer() {
