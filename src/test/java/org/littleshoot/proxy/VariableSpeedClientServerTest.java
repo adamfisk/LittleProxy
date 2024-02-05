0c7e0c335f8ee664bc9c69ac70a38295c96340ff
Andrei Solntsev <andrei.solntsev@gmail.com>
2024-02-05 23:40:32 +0200

    [refactoring] fix warning "Call to 'printStackTrace()'"

--- src/test/java/org/littleshoot/proxy/VariableSpeedClientServerTest.java
+++ src/test/java/org/littleshoot/proxy/VariableSpeedClientServerTest.java
@@ -9,8 +9,11 @@ import org.apache.http.util.EntityUtils;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -29,6 +32,7 @@ import static org.junit.Assert.assertEquals;
  */
 @Ignore
 public class VariableSpeedClientServerTest {
+    private static final Logger log = LoggerFactory.getLogger(VariableSpeedClientServerTest.class);
 
     private static final int PORT = TestUtils.randomPort();
     private static final int PROXY_PORT = TestUtils.randomPort();
@@ -44,8 +48,7 @@ public class VariableSpeedClientServerTest {
         doTest(PORT, PROXY_PORT, true);
     }
 
-    private void doTest(int port, int proxyPort, boolean slowServer)
-            throws Exception {
+    private void doTest(int port, int proxyPort, boolean slowServer) throws Exception {
         startServer(port, slowServer);
         Thread.yield();
         DefaultHttpProxyServer.bootstrap().withPort(proxyPort).start();
@@ -53,8 +56,7 @@ public class VariableSpeedClientServerTest {
         Thread.sleep(400);
         try (CloseableHttpClient client = TestUtils.createProxiedHttpClient(proxyPort)) {
 
-            System.out
-              .println("------------------ Memory Usage At Beginning ------------------");
+            log.info("------------------ Memory Usage At Beginning ------------------");
             TestUtils.getOpenFileDescriptorsAndPrintMemoryUsage();
 
             final String endpoint = "http://127.0.0.1:" + port + "/";
@@ -98,8 +100,7 @@ public class VariableSpeedClientServerTest {
             assertEquals(CONTENT_LENGTH, bytesRead);
             // final String body = IOUtils.toString(entity.getContent());
             EntityUtils.consume(entity);
-            System.out
-              .println("------------------ Memory Usage At Beginning ------------------");
+            log.info("------------------ Memory Usage At Beginning ------------------");
             TestUtils.getOpenFileDescriptorsAndPrintMemoryUsage();
         }
     }
@@ -108,16 +109,15 @@ public class VariableSpeedClientServerTest {
         final Thread t = new Thread(() -> {
             try {
                 startServerOnThread(port, slowReader);
-            } catch (Exception e) {
-                e.printStackTrace();
+            } catch (IOException e) {
+                log.error("Failed to start server on port {} (slowReader: {})", port, slowReader, e);
             }
         }, "Test-Server-Thread");
         t.setDaemon(true);
         t.start();
     }
 
-    private void startServerOnThread(int port, boolean slowReader)
-            throws Exception {
+    private void startServerOnThread(int port, boolean slowReader) throws IOException {
         try (ServerSocket server = new ServerSocket(port)) {
             server.setSoTimeout(100000);
             final Socket sock = server.accept();
@@ -128,32 +128,29 @@ public class VariableSpeedClientServerTest {
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
             while (br.read() != 0) {
             }
-            final OutputStream os = sock.getOutputStream();
-            final String responseHeaders =
-                    "HTTP/1.1 200 OK\r\n" +
-                            "Date: Sun, 20 Jan 2013 00:16:23 GMT\r\n" +
-                            "Expires: -1\r\n" +
-                            "Cache-Control: private, max-age=0\r\n" +
-                            "Content-Type: text/html; charset=ISO-8859-1\r\n" +
-                            "Server: gws\r\n" +
-                            "Content-Length: " + CONTENT_LENGTH + "\r\n\r\n"; // 10
-            // gigs
-            // or
-            // so.
+            try (final OutputStream os = sock.getOutputStream()) {
+                final String responseHeaders =
+                  "HTTP/1.1 200 OK\r\n" +
+                  "Date: Sun, 20 Jan 2013 00:16:23 GMT\r\n" +
+                  "Expires: -1\r\n" +
+                  "Cache-Control: private, max-age=0\r\n" +
+                  "Content-Type: text/html; charset=ISO-8859-1\r\n" +
+                  "Server: gws\r\n" +
+                  "Content-Length: " + CONTENT_LENGTH + "\r\n\r\n"; // ~10 gigs
 
-            os.write(responseHeaders.getBytes(UTF_8));
+                os.write(responseHeaders.getBytes(UTF_8));
 
-            int bufferSize = 100000;
-            final byte[] bytes = new byte[bufferSize];
-            Arrays.fill(bytes, (byte) 77);
-            int remainingBytes = CONTENT_LENGTH;
+                int bufferSize = 100000;
+                final byte[] bytes = new byte[bufferSize];
+                Arrays.fill(bytes, (byte) 77);
+                int remainingBytes = CONTENT_LENGTH;
 
-            while (remainingBytes > 0) {
-                int numberOfBytesToWrite = Math.min(remainingBytes, bufferSize);
-                os.write(bytes, 0, numberOfBytesToWrite);
-                remainingBytes -= numberOfBytesToWrite;
+                while (remainingBytes > 0) {
+                    int numberOfBytesToWrite = Math.min(remainingBytes, bufferSize);
+                    os.write(bytes, 0, numberOfBytesToWrite);
+                    remainingBytes -= numberOfBytesToWrite;
+                }
             }
-            os.close();
         }
     }
 }
